196f8ba9fa65caa7924e28d340cc1e16edbc612b
timmy <>
2023-10-31 09:29:16 +0800

    fix: provide & inject ctx creating util function

--- src/utils/createContext.ts
+++ src/utils/createContext.ts
@@ -2,5 +2,5 @@ import { provide, inject } from 'vue'
 
 export const createContext = <T>(ctx: T) => {
   const storeKey = crypto.randomUUID()
-  return [() => provide(storeKey, ctx), () => inject<T>(storeKey)] as const
+  return [() => provide(storeKey, ctx), () => inject<T>(storeKey)!] as const
 }
